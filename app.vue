b0a517726c07cabc2e9262207fd61a1be6f8c384
Martins <martins.zeltins@arkbauer.com>
2023-07-24 08:45:48 +0300

    [master] Initial commit

--- app.vue
+++ app.vue
@@ -1,7 +1,10 @@
 <template>
     <nav class="p-4 md:px-6 bg-black text-white">
         <ul class="flex justify-between">
-            <NuxtLink to="/" target="">Nuxt View Transitions Demo</NuxtLink>
+            <NuxtLink to="/" target="">
+                Nuxt View Transitions Demo
+            </NuxtLink>
+
             <img class="h-5" src="https://nuxt.com/assets/design-kit/logo/full-logo-green-light.svg" />
         </ul>
     </nav>
